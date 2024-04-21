c53c8f423dfb18288ffeec05f55f33c4071a80ec
Tina Pham <tip226@lehigh.edu>
2024-04-21 16:56:59 -0400

    joymapper works

--- src/base_station_urc/src/joyMapper/joyMapper.cpp
+++ src/base_station_urc/src/joyMapper/joyMapper.cpp
@@ -1,128 +1,61 @@
-#include "stdio.h"
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/twist.hpp>
+#include <sensor_msgs/msg/joy.hpp>
 
-#include "ros/ros.h"
-#include <sstream>
-#include "cross_pkg_messages/ManualDriveCMD.h"
-#include "cross_pkg_messages/RoverComputerDriveCMD.h"
-#include "sensor_msgs/Joy.h"
+class JoyMapper : public rclcpp::Node
+{
+public:
+  JoyMapper() : Node("joy_mapper")
+  {
+    joy0_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
+      "joy0", 10, std::bind(&JoyMapper::joy0Callback, this, std::placeholders::_1));
 
-// #include "cs_libguarded/cs_libguarded.h"
+    joy1_sub_ = this->create_subscription<sensor_msgs::msg::Joy>(
+      "joy1", 10, std::bind(&JoyMapper::joy1Callback, this, std::placeholders::_1));
 
-#define TESTING_MODE 0
+    drive_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  }
 
-ros::Publisher manualDrive_pub;
-ros::Publisher manualArm_pub;
+private:
+  void joy0Callback(const sensor_msgs::msg::Joy::SharedPtr msg)
+  {
+    last_joy0_msg_ = *msg;
+    sendCommand();
+  }
 
+  void joy1Callback(const sensor_msgs::msg::Joy::SharedPtr msg)
+  {
+    last_joy1_msg_ = *msg;
+    sendCommand();
+  }
 
-// geometry_msgs::Vector3 differentialDriveMapper(double driveAxis, double turnAxis) {
+  void sendCommand()
+  {
+    if (last_joy0_msg_.axes.empty() || last_joy1_msg_.axes.empty())
+      return;
 
-// }
+    geometry_msgs::msg::Twist cmd;
+    cmd.linear.x = last_joy0_msg_.axes[1] * sensitivity_;
+    cmd.angular.z = last_joy1_msg_.axes[1] * -sensitivity_;
 
-bool gotA0MSG = false;
-bool gotA1MSG = false;
+    drive_pub_->publish(cmd);
+  }
 
+  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy0_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy1_sub_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr drive_pub_;
 
-int op_mode;
+  sensor_msgs::msg::Joy last_joy0_msg_;
+  sensor_msgs::msg::Joy last_joy1_msg_;
 
-sensor_msgs::Joy last0MSG;
-sensor_msgs::Joy last1MSG;
+  const double sensitivity_ = 0.6;
+};
 
-void joy0Callback(const sensor_msgs::JoyConstPtr& msg) {
-   gotA0MSG = true;
-   last0MSG = *msg;
-}
-void joy1Callback(const sensor_msgs::JoyConstPtr& msg) {
-   gotA1MSG = true;
-   last1MSG = *msg;
-}
-
-void sendCMD() {
-   if (!gotA0MSG || !gotA1MSG) return;
-
-   float sensitivity = 0.6; //0.40f;
-   // float sensitivity_turn = 0.4; //0.40f;
-
-
-   cross_pkg_messages::ManualDriveCMD cmd;
-   #if TESTING_MODE
-   //test in a sin wive for x and cos wave for y
-   float mag = 0.4f;
-   float period = 10.0f;
-
-   // cmd.value.x = 0.4f;
-   // cmd.value.y = 0.4f;
-   cmd.value.x = mag * sin(ros::Time::now().toSec() * 2.0f * M_PI / period);
-   cmd.value.y = mag * cos(ros::Time::now().toSec() * 2.0f * M_PI / period);
-
-   #else
-
-   // float diff = abs(last0MSG.axes[1] - last1MSG.axes[1]);
-
-   // float turn_t_val = diff / ((last0MSG.axes[1] + last1MSG.axes[1]) / 2);
-
-   // float sensitivity = sensitivity_streight + ((sensitivity_turn - sensitivity_streight) * turn_t_val);
-
-   cmd.value.x = last0MSG.axes[1] * sensitivity;
-   //temp inverting before fixing motroctr
-   cmd.value.y = last1MSG.axes[1] * -sensitivity;
-   #endif
-   cmd.value.z = 0; // this is ignored
-
-   // send the command
-   if (!op_mode) {
-      manualDrive_pub.publish(cmd);
-   }
-   // ROS_INFO("sending msg");
-
-   if (op_mode)
-   {
-      cross_pkg_messages::RoverComputerDriveCMD armCMD;
-      // set the CMD_L x y z to the last0MSG axes 0 1 2
-      // update, x is pitch, y = roll, z = yaw
-      armCMD.CMD_L.x = last0MSG.axes[1];
-      armCMD.CMD_L.y = last0MSG.axes[0];
-      armCMD.CMD_L.z = last0MSG.axes[2];
-      // set the CMD_R x y z to the last1MSG axes 0 1 2
-      armCMD.CMD_R.x = last1MSG.axes[1];
-      armCMD.CMD_R.y = last1MSG.axes[0];
-      armCMD.CMD_R.z = last1MSG.axes[2];
-
-      manualArm_pub.publish(armCMD);
-   }
-}
-
-int main(int argc, char** argv) {
-    
-   ros::init(argc, argv, "JoyMapper"); 
-
-   ros::NodeHandle n;
-
-   //TOOO: set rate to correct amount
-   ros::Rate loop_rate(30); 
-
-   ROS_INFO("Joy Mapper is running");
-   
-   manualDrive_pub = n.advertise<cross_pkg_messages::ManualDriveCMD>("manualCommands", 10);
-   manualArm_pub = n.advertise<cross_pkg_messages::RoverComputerDriveCMD>("manualArmControl", 10);
-
-   //register callbacks 
-
-   //the queue is set very small so the latest command is always used
-   ros::Subscriber sub1 = n.subscribe("joy0", 1000, joy0Callback);
-   ros::Subscriber sub2 = n.subscribe("joy1", 1000, joy1Callback);
-
-
-   while (ros::ok()) {
-      //proccsing
-
-      ros::param::get("/op_mode", op_mode);
-
-      sendCMD();
-
-      //run loop
-      ros::spinOnce();
-      loop_rate.sleep();
-   }
-
-   return 0;
+int main(int argc, char** argv)
+{
+  rclcpp::init(argc, argv);
+  auto joy_mapper = std::make_shared<JoyMapper>();
+  rclcpp::spin(joy_mapper);
+  rclcpp::shutdown();
+  return 0;
 }
