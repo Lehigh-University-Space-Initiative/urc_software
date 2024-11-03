41797bb5830f128b28a4dd25e148d5198c278618
Tina Pham <tip226@lehigh.edu>
2024-11-03 14:01:34 -0500

    removed comment in base_station

--- run_nodes.sh
+++ run_nodes.sh
@@ -17,8 +17,7 @@ case "$1" in
     wait
     ;;
   base_station)
-    # ros2 launch base_station_urc base_station_launch.py
-    ros2 run base_station_urc GroundStationGUI
+    ros2 launch base_station_urc base_station_launch.py
     ;;
   main_computer)
     ros2 launch main_computer_urc main_computer_launch.py
