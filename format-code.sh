0640f889cc9d31b2dae710380120447996d37594
Anders Heyniger <anders.heyniger@gmail.com>
2025-04-20 12:52:29 -0400

    created script to clang-format the directory. currently, it goes based on the current directory. run it alone to just give warnings about whether or not it is formatted correctly, or run with -i to in-place format the directory

--- format-code.sh
+++ format-code.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
-cd "$(dirname "$0")"
+INPLACE=false
+TARGET_DIR="."
 
-if ["$1" == "-i"]; then
-    clang-format
+if [[ "$1" == "-i" ]]; then
+    INPLACE=true
+fi
+
+FILES=$(find "$TARGET_DIR" -type f \( -name "*.cpp" -o -name "*.h" \))
+
+if $INPLACE; then
+    echo "Formatting files in-place..."
+    clang-format -i $FILES
+    echo "Done."
+    exit 0
 else
+    echo "Running clang-format in dry-run mode..."
 
+    HAS_ERRORS=0
+    for FILE in $FILES; do
+        DIFF=$(clang-format --dry-run --Werror "$FILE" 2>&1)
+        if [[ $? -ne 0 ]]; then
+            echo "Formatting issue in: $FILE"
+            echo "$DIFF"
+            HAS_ERRORS=1
+        fi
+    done
+
+    if [[ $HAS_ERRORS -eq 1 ]]; then
+        echo "Some files do not meet clang-format standards."
+        exit 1
+    else
+        echo "All files are properly formatted."
+        exit 0
+    fi
 fi
